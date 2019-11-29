a4042080e312f09a51811f674199beb8f1a56c45
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-11-29 16:09:38 +0530

    new change

--- first_form.php
+++ first_form.php
@@ -6,7 +6,7 @@
 		
 		$to='livexcellence1@gmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Prestige Smart City';
-		$message="ProjectName :"."Prestige smart City"."\n"."Name :".$name."\n"."Phone :".$phone."\n";
+		$message="ProjectName :"."Prestige smart City"."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Email :".$email."\n";
 		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
 			echo "<h1 style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
