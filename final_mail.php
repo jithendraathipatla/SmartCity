1192bff9c4180fb87d459d0ea6951066442267e6
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-12-13 07:11:27 +0530

    second push

--- final_mail.php
+++ final_mail.php
@@ -9,29 +9,24 @@
 		$message="Project_Name:"."Prestige Smart City"."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Email :".$email."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
-							
-			echo "<h1  style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-			
-			//Enter your login username 
-			$username="Jithendra";
-	
-	        $password="Jithendra@18";
-	
-	       $message1="Property_Name :".$subject."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-	
-	
-	           $sender="TestID";
-	
-	
-	          $mobile_number=9071352854;
-	if($_POST['submitted']=='true')
-         { 
-		$url="login.bulksmsgateway.in/sendmessage.php?user=".urlencode($username)."&password=".urlencode($password)."&mobile=".urlencode($number)."&sender=".urlencode($sender)."&message=".urlencode($message1)."&type=".urlencode('3'); 
-		$ch = curl_init($url);
+		$apiKey = urlencode("PC56D9fMjaQ-3gw9h0qV4d5KkwYZMpo5xr1McpRBYv");
+		// Message details
+		$numbers = array(918050933931);
+		$sender = urlencode("TXTLCL");
+		$message = rawurlencode("$message");
+
+		$numbers = implode(",", $numbers);
+
+		// Prepare data for POST request
+		$data = array("apikey" => $apiKey, "numbers" => $numbers, "sender" => $sender, "message" => $message);
+		// Send the POST request with cURL
+		$ch = curl_init("https://api.textlocal.in/send/");
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		echo $curl_scraped_page = curl_exec($ch);
-		curl_close($ch); 
-}
+		$response = curl_exec($ch);
+		curl_close($ch);	
+	    echo "<h1  style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 		}
 		
 	}
